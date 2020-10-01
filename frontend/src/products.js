9a24777f045bc1363d850f1e52a999fab8c44c2e
Thirstypooch <thirstypooch@protonmail.com>
2020-09-30 23:10:24 -0500

    header and footer

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: '1 era Línea Óleo de Defensa',
+    image: '/images/antioxidant.jpg',
+    description:
+      'De textura ligera, fácil absorción y desinflamante está elaborado con 12 ingredientes ricos en antioxidantes , los mismos que ayudan a neutralizar los odiosos radicales libres',
+    brand: 'YOU Organic Skincare',
+    category: 'Skincare',
+    price: 29.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'The Oil Cleansing Method',
+    image: '/images/bifasicoIncienso.jpg',
+    description:
+      'Por eso creamos este maravilloso demaquillante nutritivo ✨Bifásico de Incienso✨su formulación es 50% base de aceites puros prensados en frío y 50 % base de hidrosol, las dos fases se juntan y activan sus funciones💫nutrir tu piel, disolver la grasa acumulada en el rostro y eliminar toxinas e impurezas incrustadas en los poros',
+    brand: 'YOU Organic Skincare',
+    category: 'Skincare',
+    price: 19.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: '🍃Súper Food Mask 🍃 ',
+    image: '/images/foodmask.jpg',
+    description:
+      'Súper Food Mask es un súper alimento para tu piel✨ es revitalizante ,ayuda a descongestionar , desinflamar , regenerar y desintoxicar la piel. A base de clorofila, espinacas y camu camu (este es fruto nativo de la Amazonia 🌳🌴🎍- es antiinflamatorio, emoliente y con gran contenido de vitamina C)',
+    brand: 'YOU Organic Skincare',
+    category: 'Skincare',
+    price: 39.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: '🌸Rodillo Facial 🌸 ',
+    image: '/images/rodillo.jpg',
+    description:
+      'Mejora la circulación sanguínea y la elasticidad de la piel. ✨Promueve el drenaje linfático. ✨Reduce la hinchazón y las arrugas. ✨Relaja. ✨Elimina toxinas. ✨Reduce la oscuridad debajo de los círculos oculares. ✨Desinflama',
+    brand: 'YOU Organic Skincare',
+    category: 'Skincare',
+    price: 19.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: '🌷Luxury Rose Mask🌷',
+    image: '/images/rosemask.jpg',
+    description:
+      'Es nuestra mascarilla a base de rosas pulverizadas y arcilla rosa ideal para cutis seco, sensible y maduro, por su alto contenido de sílice, antioxidantes y minerales purifica, revitaliza , aporta suavidad y sedosidad a la piel🧖🏻‍♀️ Esta mascarilla es muy versátil la puedes mezclar con agua de la llave💧yogurt , leche vegetal🥛jugo de aloe Vera 🌵, te de manzanilla , miel🍯 óleo Gleam y/o 1era Linea.',
+    brand: 'YOU Organic Skincare',
+    category: 'Skincare',
+    price: 29.99,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: '🍊 Vita-C',
+    image: '/images/vitac.jpg',
+    description:
+      'Es una exquisita crema reparadora de la piel riquísima en Antioxidantes. Úsala en el día antes del bloqueador solar 🧴 Te va a encantar 🙌🏻',
+    brand: 'YOU Organic Skincare',
+    category: 'Skincare',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products

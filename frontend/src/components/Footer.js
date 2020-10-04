d28295f37b0ff6d88bead2eca3d18a1d1b76823e
Thirstypooch <thirstypooch@protonmail.com>
2020-10-03 21:33:23 -0500

    getting the card ready

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Bon o Bon</Col>
+          <Col className='text-center py-3'>Copyright &copy; You Organic Skincare</Col>
         </Row>
       </Container>
     </footer>
